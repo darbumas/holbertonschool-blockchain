4da7f321346dd76eda7c7867a39b1fa06a385909
darbumas <2314@holbertonschool.com>
2023-05-27 17:52:00 +0000

    fix mixed declarations

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -3,10 +3,10 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
+	SHA256_CTX context;
+
 	if (!digest)
 		return (NULL);
-
-	SHA256_CTX context;
 
 	if (!SHA256_Init(&context))
 		return (NULL);
