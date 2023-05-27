708db6d4fb205e493cc2a1f2ccbb56cf1e9fdcf5
darbumas <2314@holbertonschool.com>
2023-05-27 18:17:31 +0000

    EC_KEY to pub key

---
+++ crypto/ec_to_pub.c
@@ -0,0 +1,27 @@
+#include "hblk_crypto.h"
+
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
+{
+	const EC_POINT *pub_key;
+	const EC_GROUP *group;
+	size_t pub_len;
+
+	if (!key || !pub)
+		return (NULL);
+
+	pub_key = EC_KEY_get0_public_key(key);
+	if (!pub_key)
+		return (NULL);
+
+	group = EC_KEY_get0_group(key);
+	if (!group)
+		return (NULL);
+
+	/* Convert the pub key to a binary and store it in the pub array */
+	pub_len = EC_POINT_point2oct(group, pub_key,
+			POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
+	if (pub_len == 0)
+		return (NULL);
+
+	return (pub);
+}
