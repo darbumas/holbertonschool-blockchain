a3ade0ed12ada296beb2675cb43e9dfd7a1811ad
darbumas <2314@holbertonschool.com>
2023-06-09 18:49:37 +0000

    betty warnings

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -41,16 +41,16 @@ void hex2bin(const char *src, uint8_t *target)
  */
 blockchain_t *blockchain_create(void)
 {
-	block_t *genesis_block;
-	char *genesis_data;
-	char *genesis_hash;
-	blockchain_t *block;
+	block_t *genesis_block = NULL;
+	char *genesis_data = "Holberton School";
+	char *genesis_hash =
+	"c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803";
+	blockchain_t *block = NULL;
 
 	block = malloc(sizeof(blockchain_t));
 	if (!block)
 		return (NULL);
 
-	/* Create the linked list for the blockchain's chain */
 	block->chain = llist_create(MT_SUPPORT_FALSE);
 	if (!block->chain)
 	{
@@ -64,29 +64,16 @@ blockchain_t *blockchain_create(void)
 		free(block);
 		return (NULL);
 	}
-	/* Initialize all fields (debugging Valgrind conditional jump) */
-	/*memset(genesis_block->hash, 0, SHA256_DIGEST_LENGTH);*/
 	memset(genesis_block, 0, sizeof(*genesis_block));
-
-	/* Initialize the genesis block with static content */
 	genesis_block->info.index = 0;
 	genesis_block->info.difficulty = 0;
 	genesis_block->info.timestamp = 1537578000;
 	genesis_block->info.nonce = 0;
 	memset(genesis_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-
-	/* Copy the data into the genesis block */
-	genesis_data = "Holberton School";
 	memcpy(genesis_block->data.buffer, genesis_data, strlen(genesis_data));
 	genesis_block->data.buffer[strlen(genesis_data)] = '\0';
 	genesis_block->data.len = strlen(genesis_data);
-
-	/* Convert the string representation of the hash into bytes */
-	genesis_hash =
-	"c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803";
 	hex2bin(genesis_hash, genesis_block->hash);
-
-	/* Add the genesis block to the blockchain's chain */
 	if (llist_add_node(block->chain, genesis_block, ADD_NODE_REAR) != 0)
 	{
 		llist_destroy(block->chain, 1, NULL);
