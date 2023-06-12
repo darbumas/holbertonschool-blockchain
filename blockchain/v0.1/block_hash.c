71cef44d15e3c21c25eebbbb00f4c8dc898e2f58
darbumas <2314@holbertonstudents.com>
2023-06-12 03:00:19 +0000

    refactoring

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,28 +1,47 @@
 #include "blockchain.h"
 
 /**
+ * compute_SHA256 - Compute the SHA256 hash
+ * @data: input buffer
+ * @len: length of the data
+ * @hash: output buffer to store the resulting hash
+ */
+void compute_SHA256(const uint8_t *data, size_t len,
+		uint8_t hash[SHA256_DIGEST_LENGTH])
+{
+	SHA256_CTX sha_ctx;
+
+	SHA256_Init(&sha_ctx);
+	SHA256_Update(&sha_ctx, data, len);
+	SHA256_Final(hash, &sha_ctx);
+}
+
+/**
  * block_hash - Computes the hash of a Block
  * @block: points to the Block to be hashed
- * @hush_buf: byte array to store the resulting hash
+ * @hash_buf: byte array to store the resulting hash
  *
  * Return: pointer to the hush buffer it is stored in.
  */
 uint8_t *block_hash(block_t const *block,
 		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	SHA256_CTX sha256;
-	block_t tmp_block;
+	size_t len;
+	uint8_t *buf;
 
-	/* Prepare a temp block_info that has zeroed hash field */
-	tmp_block = *block;
+	if (!block || !hash_buf)
+		return (NULL);
 
-	/* Zero out entire temp block structure (debugging valgrind) */
-	memset(&tmp_block, 0, sizeof(block_t));
-	memset(tmp_block.hash, 0, SHA256_DIGEST_LENGTH);
+	len = sizeof(block->info) + block->data.len;
+	buf = calloc(1, len);
+	if (!buf)
+		return (NULL);
 
-	SHA256_Init(&sha256);
-	SHA256_Update(&sha256, &tmp_block, sizeof(block_t));
-	SHA256_Final(hash_buf, &sha256);
+	memcpy(buf, &(block->info), sizeof(block->info));
+	memcpy(buf + sizeof(block->info), block->data.buffer, block->data.len);
+
+	compute_SHA256(buf, len, hash_buf);
+	free(buf);
 
 	return (hash_buf);
 }
