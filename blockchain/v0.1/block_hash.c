e7a932a5615690a36007055dc9015d89a09ac58c
darbumas <2314@holbertonstudents.com>
2023-06-12 01:48:22 +0000

    hash block

---
+++ blockchain/v0.1/block_hash.c
@@ -0,0 +1,25 @@
+#include "blockchain.h"
+
+/**
+ * block_hash - Computes the hash of a Block
+ * @block: points to the Block to be hashed
+ * @hush_buf: byte array to store the resulting hash
+ *
+ * Return: pointer to the hush buffer it is stored in.
+ */
+uint8_t *block_hash(block_t const *block,
+		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	SHA256_CTX sha256;
+	block_t tmp_block;
+
+	/* Prepare a temp block_info that has zeroed hash field */
+	tmp_block = *block;
+	memset(tmp_block.hash, 0, SHA256_DIGEST_LENGTH);
+
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, &tmp_block, sizeof(block_t));
+	SHA256_Final(hash_buf, &sha256);
+
+	return (hash_buf);
+}
