0dfbd0b5cbf3ccf4c36931f0d79d3814eb6e296e
darbumas <2314@holbertonstudents.com>
2023-07-04 19:38:27 +0000

    create trxn output

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,7 +1,7 @@
 #include "transaction.h"
 
 /**
- * tx_out_create - Allocates and initializes a transaction output structure
+ * tx_out_create - Allocates and initializes a transactionn output structure
  * @amount:	the amount of the transaction
  * @pub:	the public key of the transaction receiver
  *
@@ -9,5 +9,20 @@
  */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
+	tx_out_t *trxn = NULL;
 
+	trxn = calloc(1, sizeof(tx_out_t));
+
+	if (!trxn)
+		return (NULL);
+
+	trxn->amount = amount;
+	memcpy(trxn->pub, pub, sizeof(trxn->pub));
+	if (!sha256((int8_t const *)trxn, sizeof(trxn->amount) +
+				sizeof(trxn->pub), trxn->hash))
+	{
+		free(trxn);
+		return (NULL);
+	}
+	return (trxn);
 }
